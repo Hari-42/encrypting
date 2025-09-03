c37d24f268b696de590e26d38ec09dca0cb3f9cd
im23b-mohanathash <mohanathash@bzz.ch>
2025-09-03 09:40:13 +0200

    it works but not it is not case sensitive

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 eingabe = input("Eingabe:")
 shift = int(input("Shift:"))
+check = int(input("Do you want to encrypt it type 1, decrypt? type 2:"))
 
 alphabetlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letterslist = list(eingabe)
 
-print(letterslist)
+result = ""
+
+
+for letter in letterslist:
+
+    if letter in alphabetlist:
+        index = alphabetlist.index(letter)
+
+        if check == 1:
+            new_index = (index + shift) % 26
+
+        elif check == 2:
+            new_index = (index - shift) % 26
+
+        result += alphabetlist[new_index]
+
+    else:
+        result += letter
+
+print(result)
