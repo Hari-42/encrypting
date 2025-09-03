2faa719389b1e9cb4391ae24535f95fa20993dc8
im23b-mohanathash <mohanathash@bzz.ch>
2025-09-03 09:07:30 +0200

    the input word will be splitted in letters

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 eingabe = input("Eingabe:")
 shift = int(input("Shift:"))
 
-print(eingabe,shift)
+alphabetlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letterslist = list(eingabe)
+
+print(letterslist)
